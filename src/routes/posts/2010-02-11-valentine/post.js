869e2f3c46e132bdb386abce7cde8bc9a53aecf5
unknown <shahnazajmeri@gmail.com>
2019-12-04 22:29:00 -0500

    added a few blog posts

---
+++ src/routes/posts/2010-02-11-valentine/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `In love with the idea of love`,
+  // tags: ['Shaukat', 'blog', 'Eid'],
+  spoiler: "Valentine’s Day. Funny that they would dedicate only one day to love. If it were in my power I would devote every moment to it. Because what could be better than love. Even so, it is a good idea to have such “days” to celebrate what is generally taken for granted. It breaks the routine and sets the humdrum to a different beat. The cynic in me would dismiss it all as a marketing gimmick.",
+  getContent: () => import('./document.mdx'),
+}
