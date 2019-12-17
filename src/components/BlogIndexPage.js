3db4a512d352d1dac2608b84d9b925cee63a1ca0
unknown <shahnazajmeri@gmail.com>
2019-12-16 21:45:07 -0500

    Adding styling

--- src/components/BlogIndexPage.js
+++ src/components/BlogIndexPage.js
@@ -9,20 +9,7 @@ import styles from './BlogIndexPage.module.css'
 function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
   return (
     <div>      
-      {/* <header>
-        <h1 className={styles.title}>
-          <Link href={blogRoot}>{siteMetadata.title}</Link>
-        </h1>
-       
-      </header>
-      <div>
-        <Link href="/">Home</Link> &bull; 
-        <Link href="/blog">Blog</Link> &bull;
-        <Link href="/about">About</Link> &bull; 
-        <Link href="/tags">Tags</Link>{' '}
-          &bull;{' '}  
-          
-      </div> */}
+     
       {/* <Bio /> */}
       <ul className={styles.articlesList}>
         {postRoutes.map(route => (
@@ -38,16 +25,7 @@ function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
           pageNumber={pageNumber}
         />
       )}
-      {/* <footer className={styles.footer}>
-        <div>
-          <a href="/rss.xml" target="_blank" style={{ float: 'right' }}>
-            RSS
-          </a>
-          <Link href="/about">About</Link> &bull; <Link href="/tags">Tags</Link>{' '}
-          &bull;{' '}
-         
-        </div>
-      </footer> */}
+    
     </div>
   )
 }
--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -14,42 +14,43 @@ function BlogLayout({ blogRoot, isViewingIndex }) {
   let loadingRoute = useLoadingRoute()
 
   return (
-    <div className={styles.container}>
-      <LoadingIndicator active={!!loadingRoute} />
-
-      {// Don't show the header on index pages, as it has a special header.
-      // !isViewingIndex && (
-        <header>
-          <h3 className={styles.title}>
-            <Link href="/">{siteMetadata.title}</Link>
-          </h3>
-        </header>
-      // )
-      }
-      <div>
-        {/* <Link href="/">Home</Link> &bull;  */}
-        <Link href="/blog">Blog</Link> &bull;
-        <Link href="/articleList">Articles</Link> &bull;
-        <Link href="/book">Book</Link> &bull; 
-        <Link href="/about">About</Link> &bull; 
-        {/* <Link href="/tags">Tags</Link>{' '} */}
-        <Link href="/contact">Contact</Link> &bull;          
+    <div className={styles.outer}>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <header>
+            <h3 className={styles.title}>
+              <Link href="/">{siteMetadata.title}</Link>
+            </h3>
+          </header>
+        
+          <nav>
+            {/* <Link href="/">Home</Link> &bull;  */}
+            <Link href="/blog">Blog</Link> 
+            <Link href="/articleList">Articles</Link> 
+            <Link href="/book">Book</Link>  
+            <Link href="/about">About</Link> 
+            {/* <Link href="/tags">Tags</Link>{' '} */}
+            <Link href="/contact">Contact</Link>          
+          </nav>
+        </div>      
       </div>
-      <main>
-        <NotFoundBoundary render={() => <NotFoundPage />}>
-          <View />
-        </NotFoundBoundary>
-      </main>
-      <footer className={styles.footer}>
-        <div>        
-          <Link href="/blog">Blog</Link> &bull;
-          <Link href="/articleList">Articles</Link> &bull; 
-          <Link href="/book">Book</Link> &bull; 
-          <Link href="/about">About</Link> &bull; 
-          {/* <Link href="/tags">Tags</Link>{' '} */}
-          <Link href="/contact">Contact</Link> &bull; 
-        </div>
-      </footer>
+      <div className={styles.container}>
+        <main>
+          <NotFoundBoundary render={() => <NotFoundPage />}>
+            <View />
+          </NotFoundBoundary>
+        </main>
+        <footer className={styles.footer}>
+          <div>        
+            <Link href="/blog">Blog</Link> &bull;
+            <Link href="/articleList">Articles</Link> &bull; 
+            <Link href="/book">Book</Link> &bull; 
+            <Link href="/about">About</Link> &bull; 
+            {/* <Link href="/tags">Tags</Link>{' '} */}
+            <Link href="/contact">Contact</Link> &bull; 
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
