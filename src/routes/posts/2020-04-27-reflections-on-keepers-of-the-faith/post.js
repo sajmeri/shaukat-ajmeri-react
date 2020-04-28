cc8bdfcad27f29a1751cebf4c0cf98a5ebc49b28
SA <tamarind@SA-Central.local>
2020-04-27 20:50:02 -0400

    Added Saumaya review

---
+++ src/routes/posts/2020-04-27-reflections-on-keepers-of-the-faith/post.js
@@ -0,0 +1,7 @@
+export default {
+    title: `Reflections on Keepers of the Faith`,
+    // tags: ['Fiction', 'Novel', 'Keepers of the Faith'],
+    spoiler: "Sumaiya Hamdani, an Associate Professor of History at George Mason University, comes from a long line of Islamic scholars and academics from the Hamdani family. From their origins in Yemen they have over the generations made important contributions to Ismaili thought and literature. Sumaiya’s father, the late Abbas Hamdani, was a scholar of Ismaili history and in particular of a volume of the philosophical work the Rasa'il-e-Ikhwan al-Safa (Epistles of Brethren of Purity). The Hamdani family had been in possession of rare and unique books and manuscripts, amassed and copied over multiple generations, which Prof Abbas Hamdani generously donated to the Institute of Ismaili Studies in London.",
+    getContent: () =>
+        import ('./document.mdx'),
+}
