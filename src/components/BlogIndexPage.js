dd932beb16410b03cc09abc1f9d665a6af9e1ac9
SA <tamarind@SA-Central.local>
2020-05-11 22:46:59 -0400

    updated book page and css style

--- src/components/BlogIndexPage.js
+++ src/components/BlogIndexPage.js
@@ -8,35 +8,40 @@ import Pagination from './Pagination'
 import styles from './Styles.module.css'
 
 function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
-  
-  const pageHeadingArray=postRoutes[0].url.pathname.split("/");
-  
-  let pageHeadingCapitalized = pageHeadingArray[1].charAt(0).toUpperCase() + pageHeadingArray[1].slice(1)
-  if(pageHeadingCapitalized === "Posts") {
-    pageHeadingCapitalized = "Blog"
-  }
-  const category = pageHeadingCapitalized;
-  return (
-    <div>      
-     <h1 className={styles.category}>{category}</h1>
-      {/* <Bio /> */}
-      <ul className={styles.articlesList}>
-        {postRoutes.map(route => (
-          <li key={route.url.href}>
-            <ArticleSummary blogRoot={blogRoot} route={route} />
-          </li>
-        ))}
-      </ul>
-      {pageCount > 1 && pageHeadingArray[1]!=="reviews" && (
-        <Pagination
-          blogRoot={blogRoot}
-          pageCount={pageCount}
-          pageNumber={pageNumber}
-        />
-      )}
-    
-    </div>
-  )
+
+    const pageHeadingArray = postRoutes[0].url.pathname.split("/");
+
+    let pageHeadingCapitalized = pageHeadingArray[1].charAt(0).toUpperCase() + pageHeadingArray[1].slice(1)
+    if (pageHeadingCapitalized === "Posts") {
+        pageHeadingCapitalized = "Blog"
+    }
+    const category = pageHeadingCapitalized;
+    return ( <
+        div >
+        <
+        h1 className = { styles.category } > { category } < /h1> { /* <Bio /> */ } <
+        ul className = { styles.articlesList } > {
+            postRoutes.map(route => ( <
+                li key = { route.url.href } >
+                <
+                ArticleSummary blogRoot = { blogRoot }
+                route = { route }
+                /> <
+                /li>
+            ))
+        } <
+        /ul> {
+            pageCount > 1 && pageHeadingArray[1] !== "reviews" && ( <
+                Pagination blogRoot = { blogRoot }
+                pageCount = { pageCount }
+                pageNumber = { pageNumber }
+                />
+            )
+        }
+
+        <
+        /div>
+    )
 }
 
 export default BlogIndexPage
